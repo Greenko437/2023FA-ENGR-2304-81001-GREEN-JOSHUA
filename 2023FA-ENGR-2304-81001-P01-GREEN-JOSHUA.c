4204915d908c94bdbc6324829c6407919caf7a7c
Greenko437 <150077483+Greenko437@users.noreply.github.com>
2023-11-17 14:20:20 -0600

    Update 2023FA-ENGR-2304-81001-P01-GREEN-JOSHUA.c
    In this revision, I've added a comment above the "say_hello_world" function to describe what the function does briefly. Adding such comments is a good habit, especially as programs become larger and more complex.

--- 2023FA-ENGR-2304-81001-P01-GREEN-JOSHUA.c
+++ 2023FA-ENGR-2304-81001-P01-GREEN-JOSHUA.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
- /* 2023FA-ENGR-2304-81001-P01-GREEN-JOSHUA */
+/* 2023FA-ENGR-2304-81001-P01-GREEN-JOSHUA */
 
+// Function to display "hello, world!" message
 void say_hello_world() {
     printf("hello, world!\n");
 }
